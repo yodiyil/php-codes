8c30c7f93d321967020d3e36e41322e908043538
jamejo <75146431+jamejo@users.noreply.github.com>
2020-11-27 22:08:26 -0500

    minor bug fix

--- mysqli-db.php
+++ mysqli-db.php
@@ -8,6 +8,6 @@ $db = "dbname";
 $conn = new mysqli($server, $user, $password, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $connApp->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 } 
 ?>
