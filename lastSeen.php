80f70bb937e13d170dcf24e941d20ea0ae4f6540
jamejo <75146431+jamejo@users.noreply.github.com>
2020-11-27 21:34:40 -0500

    Update lastSeen.php

--- lastSeen.php
+++ lastSeen.php
@@ -1,28 +1,30 @@
 <?php
+
+//start usage example
+echo ago('xxxxxxxxx');
+//end usage example
+
 function ago($mtime){
 	$xtime = time() - $mtime;
-	if ($xtime < 1)
-	{
+	if ($xtime < 1){
 		return '0 seconds';
 	}
 
-	$a = array( 12 * 30 * 24 * 60 * 60  =>  'year',
-				30 * 24 * 60 * 60       =>  'month',
-				24 * 60 * 60            =>  'day',
-				60 * 60                 =>  'hour',
-				60                      =>  'minute',
-				1                       =>  'second'
-				);
+	$a = array(
+		12 * 30 * 24 * 60 * 60  =>  'year',
+		30 * 24 * 60 * 60       =>  'month',
+		24 * 60 * 60            =>  'day',
+		60 * 60                 =>  'hour',
+		60                      =>  'minute',
+		1                       =>  'second'
+	);
 
-	foreach ($a as $secs => $str)
-	{
+	foreach ($a as $secs => $str){
 		$d = $xtime / $secs;
-		if ($d >= 1)
-		{
+		if ($d >= 1){
 			$r = round($d);
 			return $r . ' ' . $str . ($r > 1 ? 's' : '') . ' ago';
 		}
 	}
 }
-
 ?>
